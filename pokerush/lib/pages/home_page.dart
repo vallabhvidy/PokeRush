6b3a0daede7cd2e07598bf7e452468ba67938b05
Vallabh Vidyasagar <vallabhvidyasagar10@gmail.com>
2024-07-15 21:04:53 +0530

    try using pokedex plugin

--- pokerush/lib/pages/home_page.dart
+++ pokerush/lib/pages/home_page.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/pokedex.dart';
+import 'package:pokerush/pokeservice.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  List<PokemonSpecies> pokemons = [];
+
+  Future getPokemons() async {
+    var dex = Pokedex();
+    for (int i=0; i < 10; i++) {
+      pokemons.add(await dex.pokemonSpecies.get(id: i+1));
+      print(pokemons[i].toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: const Padding(
@@ -20,6 +34,17 @@ class HomePage extends StatelessWidget {
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search)), SizedBox(width: 11)],
       ),
       drawer: Drawer(),
+      body: FutureBuilder(future: getPokemons(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ListView.builder(
+            itemCount: 10,
+            itemBuilder:(context, index) {
+            return ListTile(title: Text(pokemons[index].name),);
+          },);
+        } else {
+          return CircularProgressIndicator();
+        }},),
     );
   }
 }
---
+++ pokerush/lib/pokeservice.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/material.dart';
+import 'package:pokedex/pokedex.dart';
+
+class PokeService {
+  
+}
