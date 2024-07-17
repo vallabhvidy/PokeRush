3bed378088feaa5465d9aaf00ae6c824b3a9788c
Vallabh Vidyasagar <vallabhvidyasagar10@gmail.com>
2024-07-17 18:21:31 +0530

    using CachedNetworkImage instead of Image.network

--- pokerush/lib/pages/home_page.dart
+++ pokerush/lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pokedex/pokedex.dart';
 import 'package:pokerush/pokeservice.dart';
 import 'package:provider/provider.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -13,7 +14,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: const Padding(
-          padding: EdgeInsets.only(top: 11),
+          padding: EdgeInsets.only(top: 9),
           child: Image(
             image: AssetImage('lib/images/PokeRush.png'),
             height: 66,
@@ -30,10 +31,16 @@ class HomePage extends StatelessWidget {
           return CircularProgressIndicator();
         } else {
           return ListView.builder(
-            itemCount: 10,
+            itemCount: poke.pokemons.length,
             itemBuilder:(context, index) {
               return ListTile(
-                leading: CircleAvatar(backgroundImage: NetworkImage(poke.pokemons[index].img),),
+                leading: CachedNetworkImage(
+                  imageUrl: poke.pokemons[index].img,
+                  progressIndicatorBuilder: (context, url, downloadProgress) => 
+                          CircularProgressIndicator(value: downloadProgress.progress),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                  height: 50,
+                ),
                 title: Text(poke.pokemons[index].name));
             },);
         }
