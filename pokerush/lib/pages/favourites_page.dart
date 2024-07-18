de8b358dbdb61a5b3c39b3d489c2bbda1b847bb8
Vallabh Vidyasagar <vallabhvidyasagar10@gmail.com>
2024-07-19 00:05:07 +0530

    final layout

---
+++ pokerush/lib/pages/favourites_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:pokerush/pokeservice.dart';
+import 'package:pokerush/widgets/poke_card.dart';
+import 'package:provider/provider.dart';
+
+
+class FavouritesPage extends StatelessWidget {
+  const FavouritesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Padding(
+          padding: EdgeInsets.only(top: 9),
+          child: Text("F A V O U R I T E S", style: TextStyle(fontSize: 20),)
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Consumer<PokeService>(builder: (context, value, child) {
+        if (value.favourites.isEmpty) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Flexible(child: Image.asset("lib/images/EmptyBox.png", height: 225,)),
+                Padding(padding: EdgeInsets.all(8.0), child: Text("E M P T Y", style: TextStyle(fontSize: 24, color: Colors.grey)),)
+              ],
+            ),
+          );
+        } else {
+          return GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 6.0, crossAxisSpacing: 6.0), 
+              padding: const EdgeInsets.all(12.0),
+              itemCount: value.favourites.length,
+              itemBuilder: (context, index) {
+                return PokeCard(pokemon: value.favourites[index],);
+              },
+            );
+        }
+      },)
+    );
+  }
+}
--- pokerush/lib/pages/poke_screen.dart
+++ pokerush/lib/pages/poke_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:pokedex/pokedex.dart' as dex;
 import 'package:pokerush/pokeservice.dart';
 import 'package:provider/provider.dart';
@@ -11,14 +12,30 @@ class PokeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300] ,
       appBar: AppBar(
         title: Text(pokemon.name),
+        backgroundColor: Colors.transparent,
         centerTitle: true,
       ),
       body: Consumer<PokeService>(
         builder: (context, value, child) {
           if (value.currentPokemon == null) {
-            return CircularProgressIndicator();
+            if (value.internet) {
+              return Center(child: LoadingAnimationWidget.inkDrop(color: Colors.white, size: 100));
+            } else {
+              return Center(child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('lib/images/NoInternet.gif'),
+                  const Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Text("N O  I N T E R N E T", style: TextStyle(fontSize: 20),),
+                  )
+                ],
+              ),); 
+            }
           } else {
             return Column(
               children: [
@@ -30,7 +47,7 @@ class PokeScreen extends StatelessWidget {
                 ),
                 ListTile(
                   leading: Text("Name:"),
-                  //title: Text(value.currentPokemon!.formDescriptions.single.description),
+                  title: Text(value.currentPokemon!.name),
                 )
               ],
             );
